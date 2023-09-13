191a553d441253cd6f7dac514e91edae01d809c7
Kharsekina <126934266+Kharsekina@users.noreply.github.com>
2023-09-13 19:06:31 +0700

    Add files via upload

---
+++ Home_work_3.sql
@@ -0,0 +1,42 @@
+CREATE TABLE staff (
+	id INT AUTO_INCREMENT PRIMARY KEY, 
+	firstname VARCHAR(45),
+	lastname VARCHAR(45),
+	post VARCHAR(100),
+	seniority INT, 
+	salary INT, 
+	age INT
+);
+
+INSERT INTO staff (firstname, lastname, post, seniority, salary, age)
+VALUES
+('Вася', 'Петров', 'Начальник', '40', 100000, 60),
+('Петр', 'Власов', 'Начальник', '8', 70000, 30),
+('Катя', 'Катина', 'Инженер', '2', 70000, 25),
+('Саша', 'Сасин', 'Инженер', '12', 50000, 35),
+('Иван', 'Иванов', 'Рабочий', '40', 30000, 59),
+('Петр', 'Петров', 'Рабочий', '20', 25000, 40),
+('Сидр', 'Сидоров', 'Рабочий', '10', 20000, 35),
+('Антон', 'Антонов', 'Рабочий', '8', 19000, 28),
+('Юрий', 'Юрков', 'Рабочий', '5', 15000, 25),
+('Максим', 'Максимов', 'Рабочий', '2', 11000, 22),
+('Юрий', 'Галкин', 'Рабочий', '3', 12000, 24),
+('Людмила', 'Маркина', 'Уборщик', '10', 10000, 49);
+
+SELECT * FROM staff ORDER BY salary DESC;
+
+SELECT * FROM staff ORDER BY salary;
+
+SELECT * FROM staff ORDER BY salary DESC LIMIT 5;
+
+SELECT post, sum(salary) from staff group by post;
+
+SELECT COUNT(*) FROM staff 
+WHERE post = 'Рабочий' AND age >24 AND age <=49;
+
+SELECT COUNT(DISTINCT post) FROM staff;
+
+SELECT post FROM staff 
+GROUP BY post
+HAVING avg(age) < 30;
+
